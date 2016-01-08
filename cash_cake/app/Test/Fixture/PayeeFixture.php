5c333786ec00b95a72487a11d2360de2a0a58eaf
ShiroJun <0909robinson@gimail.com>
2016-01-08 17:00:10 +0900

    バージョンSJ

---
+++ cash_cake/app/Test/Fixture/PayeeFixture.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * PayeeFixture
+ *
+ */
+class PayeeFixture extends CakeTestFixture {
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
+		'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'modified' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'payee_name' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 32, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' => 1,
+			'created' => '2016-01-07 22:55:56',
+			'modified' => '2016-01-07 22:55:56',
+			'payee_name' => 'Lorem ipsum dolor sit amet'
+		),
+	);
+
+}
